24c6248c4283dee9eb674230af09904a3bce1048
Uzytkownik <uzytkownik@kursgit.pl>
2020-08-01 11:59:36 +0200

    initial commit

---
+++ src/main/java/pl/sda/JobOfferAplication/JobOfferAplicationApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.JobOfferAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/JobOfferAplication/JobOfferAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.JobOfferAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
